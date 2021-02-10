8a8951c2798ee5c296d49c55602d43871ea5772c
Qi Zhu <rubybfm1017@gmail.com>
2021-02-10 11:57:15 -0800

    jbuilder

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-
     def new
         @user = User.new
         render :new
@@ -17,6 +16,8 @@ class UsersController < ApplicationController
         end
     end
 
+    private
+    
     def user_params
         params.require(:user).permit(:username, :password)
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,8 @@ class User < ApplicationRecord
 
     has_many :questions
 
+    has_many :answers
+
     has_many :votes
 
         
