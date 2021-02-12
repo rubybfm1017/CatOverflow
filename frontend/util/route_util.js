0471d0bef49993f24c6a846754ac0f3b48b42a91
Qi Zhu <rubybfm1017@gmail.com>
2021-02-11 19:39:01 -0800

    add route util

---
+++ frontend/util/route_util.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Route, Redirect, withRouter } from 'react-router-dom';
+
+const Auth = ({ component: Component, path, exact, signedIn}) => (
+    <Route
+      path={path}
+      exact={exact}
+      render={props =>
+        !signedIn ? <Component {...props} /> : <Redirect to="/" />
+      }
+    />
+  );
+
+const Protected = ({ component: Component, path, exact, signedIn }) => (
+    <Route
+      path={path}
+      exact={exact}
+      render={props =>
+        !signedIn ? <Redirect to="/" /> : <Component {...props} />
+      }
+    />
+  );
+  
+  const mapStateToProps = state => {
+    return { signedIn: Boolean(state.users.current_user.id != null) };
+  };
+  
+  export const AuthRoute = withRouter(
+    connect(
+      mapStateToProps,
+      null
+    )(Auth)
+  );
+
+  export const ProtectedRoute = withRouter(
+    connect(
+      mapStateToProps,
+      null
+    )(Protected)
+  );
